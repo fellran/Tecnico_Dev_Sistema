ff6a94a00571abcaa423c33385cd94904bc0c497
fellran <veromaster46@gmail.com>
2024-11-12 10:38:23 -0300

    Conteudo das aulas

---
+++ aula-17-phyton/Aula-01/Aula_1_Print.py
@@ -0,0 +1,23 @@
+"""
+Docstrin - Anotações
+"""
+
+
+# end (end - Fim do print)
+print('Uma informação importante', 'importante', end='--\n')
+
+# sep (sep - separador do print)
+print('Uma informação importante', 'importante', sep='--')
+
+# type verifica o tipo da variavel
+# int
+print(type(12))
+
+# Float (Valores decimais ou ponto flutuantes)
+print(type(1.2))
+
+# String (Caracteres letras e numero e simbolos)
+print(type('oi'))
+
+# Booleano (Verdadeiro ou Falso)
+print(type(True))
