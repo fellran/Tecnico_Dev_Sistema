6a58f89a266b38f9d29897cfd4686bcac17415ac
fellran <veromaster46@gmail.com>
2025-03-25 10:13:31 -0300

    exercicios 3

---
+++ banco de dados/DML/Prof_/aula_2_exercicicio_3.sql
@@ -0,0 +1,49 @@
+CREATE DATABASE IF NOT EXISTS db_barbearia COLLATE utf8mb4_general_ci CHARSET utf8mb4;
+
+USE db_barbearia;
+
+CREATE TABLE IF NOT EXISTS tb_cliente (
+	id_cliente INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(255) NOT NULL,
+    whatapp CHAR(13) NOT NULL,
+    email VARCHAR(80) UNIQUE
+)AUTO_INCREMENT = 1;
+
+-- DML
+INSERT INTO tb_cliente (nome, whatapp, email) VALUES('joao', '6134578965423', 'joao@aluno.senai.br');
+
+/*
+Exercico
+Cnsiderando a nossa barbearia. o Cliente pediu a elaboraão de uma nova tabela de serviços
+
+- nome do serviço
+- valor do serviço
+
+Considerando nosso contexto, cria a tabela pesquisa na internet os melhores serviços de uma barbearia e insira no banco de dados (popular) - particular
+*/
+
+CREATE TABLE IF NOT EXISTS tb_servico (
+	id_servico INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    valor FLOAT(4,2) NOT NULL
+)AUTO_INCREMENT = 1;
+
+INSERT INTO tb_servico (nome, valor) VALUES ('Lavagem', 50.80), ('Corte', 20.00), ('Hidrataçâo', 70.00), ('Barba', 20.00), ('Sobrancelha', 50.00);
+
+SELECT * FROM tb_servico;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
